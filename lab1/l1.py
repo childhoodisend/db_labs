66b3efa0d3ac7ea28e89edf04e4f08b5250b3088
childhoodisend <morozvktv@gmail.com>
2021-09-20 15:01:37 +0300

    [*] update l1.py

--- lab1/l1.py
+++ lab1/l1.py
@@ -2,7 +2,7 @@ import psycopg2
 import numpy as np
 
 conn = psycopg2.connect(
-    database="db_lab", user='postgres', password='root', host='127.0.0.1', port='5432'
+    database="db_lab", user='pahpan', password='2000', host='127.0.0.1', port='5432'
 )
 conn.autocommit = True
 cursor = conn.cursor()
@@ -14,26 +14,29 @@ colors = np.array(['black', 'white'])
 def init_chessman_table():
     print("init_chessman_table() called\n")
 
-    cid = 1
+
+    cursor.execute('''ALTER SEQUENCE chessman_cid_seq RESTART WITH 1''')
+    cursor.execute('''TRUNCATE TABLE chessman CASCADE''')
+
+
     for type in types:
         for color in colors:
-            cursor.execute('''INSERT INTO CHESSMAN(cid, type, color) VALUES ({},'{}', '{}')'''.format(cid, type, color))
-            cid += 1
+            cursor.execute('''INSERT INTO CHESSMAN(type, color) VALUES ('{}', '{}')'''.format(type, color))
 
     conn.commit()
     print("\n")
 
-
 def init_chessboard_table():
     print("init_chessboard_table() called\n")
 
-    x = np.fromiter((i for i in range(8)), dtype=int)
-    y = x
-    cids = np.fromiter((i for i in range(1, 13, 1)), dtype=int)
-    for xx in x:
-        for yy in y:
-            for cid in cids:
-                cursor.execute('''INSERT INTO CHESSBOARD(cid, x, y) VALUES ({}, {}, {})'''.format(cid, xx, yy))
+    cursor.execute('''TRUNCATE TABLE chessboard CASCADE''')
+
+    cursor.execute('''INSERT INTO Chessboard (cid, x, y) VALUES
+    (5,  'a', 8), (9,  'b', 8), (7,  'c', 8), (3,  'd', 8), (1,  'e', 8), (7,  'f', 8), (9,  'g', 8), (5,  'h', 8),
+    (11, 'a', 7), (11, 'b', 7), (11, 'c', 7), (11, 'd', 7), (11, 'e', 7), (11, 'f', 7), (11, 'g', 7), (11, 'h', 7),
+
+    (12, 'a', 2), (12, 'b', 2), (12, 'c', 2), (12, 'd', 2), (12, 'e', 2), (12, 'f', 2), (12, 'g', 2), (12, 'h', 2),
+    (6,  'a', 1), (10, 'b', 1), (8,  'c', 1), (4,  'd', 1), (2,  'e', 1), (8,  'f', 1), (10, 'g', 1), (6,  'h', 1)''')
 
     conn.commit()
     print("\n")
@@ -87,8 +90,9 @@ def types_and_count():
 
 
 def l1():
-    # init_chessman_table()
-    # init_chessboard_table()
+
+    init_chessman_table()
+    init_chessboard_table()
 
     # a
     count_all_figures()
