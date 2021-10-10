f9ef0da49054ff08d66614206657acddd059ffff
childhoodisend <morozvktv@gmail.com>
2021-10-10 19:24:04 +0300

    [*] tasks 1 - 9 done

--- lab2/task2.sql
+++ lab2/task2.sql
@@ -1,20 +1,38 @@
 -- 1 Вывести количество продавцов
+select count(*) from employees;
+
 
 -- 2 Сколько продуктов стоят меньше 10$
+select count(*) from products where price < 10.0;
+
 
 -- 3 Найти цену самого дорогого продукта
+select price from products order by price desc limit 1;
+
 
 -- 4 Найти все самые дорогие продукты (с самой большой ценой)
+select * from products where price in (select price from products order by price desc limit 1);
+
 
 -- 5 Найти количество продуктов, название которых начинается на Metal Plate
+select * from products where name like 'Metal Plate%';
 
--- 6 Найти среднюю цену продукта, в названии которых встречается слово silver
+
+-- 6 Найти среднюю цену продукта, в названии которых встречается слово Silver
+select sum(price) / count(price) from products where name like '%Silver%';
+
 
 -- 7 Сколько покупателей, которых зовут Alicia?
+select count(*) from customers where firstname = 'Alicia';
+
 
 -- 8 Сколько есть уникальных имен покупателей?
+select count(*) from customers where firstname in (select distinct firstname from customers);
+
 
 -- 9 У какого количества покупателей определена буква отчества?
+select count(*) from customers where middleinitial is not null;
+
 
 -- 10 Какая буква отчества самая популярная?
 
