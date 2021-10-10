9ab5aac5b6595d52e9851cd586fbd794d95c949c
childhoodisend <morozvktv@gmail.com>
2021-10-10 20:13:33 +0300

    [*] tasks 10 - 21 done

--- lab2/task2.sql
+++ lab2/task2.sql
@@ -35,28 +35,52 @@ select count(*) from customers where middleinitial is not null;
 
 
 -- 10 Какая буква отчества самая популярная?
+select middleinitial from (select middleinitial, count(*) as cnt from Customers where customers.middleinitial is not null group by middleinitial) as countable order by cnt desc limit 1;
+
 
 -- 11 У какого количества покупателей не определена буква отчества?
+select count (*) from customers where customers.middleinitial is null;
+
 
 -- 12 У какого количества покупателей совпали имена? Вывести статистику для каждого имени.
+select firstname, count (*) as cnt from customers group by firstName;
+
 
 -- 13 У какого количества покупателей совпали имена и отчества? Вывести статистику для каждого имени и отчества.
+select firstname, middleinitial, count (*) as cnt from customers group by (firstName, middleinitial);
+
 
 -- 14 Сколько однофамильцев среди продавцов
+select lastname, count (*) as cnt from employees group by lastname;
+
 
 -- 15 Найти количество продаж продавца по имени Ann
+select count(*) from sales left join employees e on sales.salespersonid = e.employeeid and firstname = 'Ann';
+
 
 -- 16 Вывести количество различных товаров, которые продает Ann
+select count(distinct sales.ProductID) from sales join employees e on sales.salespersonid = e.employeeid and firstname = 'Ann';
+
 
 -- 17 Сколько продаж есть у каждого продавца?
+select (firstname, middleinitial, lastname), count(*) from sales join Employees on sales.SalesPersonID = Employees.EmployeeID group by (firstname, middleinitial, lastname);
+
 
 -- 18 Сколько покупок у каждого покупателя?
+select c.customerid, count(*) from sales join customers c on sales.customerid = c.customerid group by c.customerid order by c.customerid;
+
 
 -- 19 Скольким покупателям продавал товары каждый продавец?
+select employeeid, count(distinct s.customerid) from employees join sales s on employees.employeeid = s.salespersonid group by employeeid order by employeeid;
+
 
 -- 20 Какова средняя цена каждой продажи во всем продажам?
+select AVG(price) from sales join products p on sales.productid = p.productid;
+
 
 -- 21 Какова сумма продаж у каждого продавца?
+select sales.salespersonid, SUM(price) from sales join products p on sales.productid = p.productid group by salespersonid;
+
 
 -- 22 Для каждого товара найти мат.ожидание количества в каждой продаже и среднее квадратичное отклонение.
 
