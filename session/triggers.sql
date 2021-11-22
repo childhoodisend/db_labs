8b5fad972f8ce852523be8f3839feec824114b82
childhoodisend <morozvktv@gmail.com>
2021-11-22 17:04:59 +0300

    [+] add tests and 2 trigger

--- session/triggers.sql
+++ session/triggers.sql
@@ -38,43 +38,67 @@ begin
 end;
 $tr$ language plpgsql;
 
-
 create trigger del_dep before delete on dependencies for each row execute procedure delete_dep();
-insert into dependencies (subject_id, depends_of, is_required) values (53, 6, true);
 
-delete from dependencies where subject_id=53 and depends_of=5;
-drop function if exists delete_dep() cascade;
+-- TEST
+insert into dependencies (subject_id, depends_of, is_required) values (101, 99, true);
+delete from dependencies where subject_id=101 and depends_of=102;
+-- TEST
+
+
 
 -- 2.2 Создать триггер на удаление студента. При удалении студента проверять, остались ли еще студенты в его группе.
 -- Если студентов больше не осталось, удалить группу.
 
 
-create table students_copy as (select * from students);
-create table st_groups as (select distinct group_id from students);
-create or replace function delete_st() returns trigger AS $tr$
+create or replace view st_groups as (select distinct group_id from students); -- all st groups
+
+
+create or replace function delete_st_group() returns trigger AS $tr$
 declare
-    st_cnt int = (select count(*) from st_groups join students_copy s on st_groups.group_id = s.group_id where st_groups.group_id=old.group_id);
+    st_cnt int = (select count(*) from st_groups join students s on st_groups.group_id = s.group_id where st_groups.group_id=old.group_id);
     gr_id int = (select group_id from st_groups where st_groups.group_id=old.group_id);
 begin
 
-    raise notice 'st_cnt (%), gr_id (%)', st_cnt, gr_id;
+    raise notice 'delete_st() : st_cnt (%), gr_id (%)', st_cnt, gr_id;
 
-    if(st_cnt = 0) then
-        raise notice 'st_cnt (%), gr_id (%) : can delete', st_cnt, gr_id;
+    if(st_cnt = 1) then
+        raise notice 'delete_st() : can delete';
         delete from st_groups where group_id=gr_id;
-        return new;
+        return old;
     else
-        raise notice 'nothing to delete';
+        raise notice 'delete_st() : st group is not deleted';
         return old;
     end if;
 
-end;
-$tr$ language plpgsql;
+end; $tr$ language plpgsql;
+
+create trigger del_st_gr before delete on students for each row execute procedure delete_st_group();
+
+create or replace function delete_st_results() returns trigger AS $tr$
+begin
 
 
-create trigger del_st after delete on students_copy for each row execute procedure delete_st();
-insert into students_copy(id, firstname, middle_name, lastname, book, group_id) values (27, 'testtt', 'tsdsdt', 'sdfds', 2124, 1);
-insert into st_groups(group_id) values (1);
-delete from students_copy where id=24;
+    raise notice 'delete_st_results() : delete results for (%)', old.id;
+    delete from results where student_id=old.id;
+    return old;
 
-select count(*) from st_groups join students_copy s on st_groups.group_id = s.group_id where st_groups.group_id=1;
+end; $tr$ language plpgsql;
+
+create trigger del_st_res before delete on students for each row execute procedure delete_st_results();
+
+
+-- TEST
+insert into students(id, firstname, middle_name, lastname, book, group_id) values (25, 'NAME1', 'MNAME1', 'LNAME1', 125, 12);
+insert into results (student_id, subject_id, attempt, status, is_required) values (25, 18, '1', 'need', 'true');
+insert into students(id, firstname, middle_name, lastname, book, group_id) values (26, 'NAME2', 'MNAME2', 'LNAME2', 126, 12);
+insert into results (student_id, subject_id, attempt, status, is_required) values (26, 18, '1', 'need', 'true');
+insert into students(id, firstname, middle_name, lastname, book, group_id) values (27, 'NAME3', 'MNAME3', 'LNAME3', 127, 12);
+insert into results (student_id, subject_id, attempt, status, is_required) values (27, 18, '1', 'need', 'true');
+
+delete from students where students.id in (25, 26, 27);
+-- TEST
+
+
+create or replace rule delete_st_group_rule as on delete to st_groups
+do instead select distinct group_id from students where group_id <> old.group_id;
